479b2b38640c468af86e8a7be9034f1b8027b4d9
Jared Kroke <jared.kroke@gmail.com>
2022-01-07 17:09:56 -0600

    Working on making the table look nice and adding jquery functionality to show output on the DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
 $(document).ready(readyNow);
 
+let totalMonthlyCosts = 0;
+
+
 function readyNow() {
     console.log('JQuery is loaded!');
+    $('#addEmployee').on('click', addEmployee);
+}
+
+function addEmployee() {
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let idNumber = $('#idNumber').val();
+    let jobTitle = $('#title').val();
+    let annualSalary = $('#annualSalary').val();
+    if (firstName === '' || lastName === '' || idNumber === '' || jobTitle === '' || annualSalary === '') {
+        alert('Please fill out all inputs before submitting.');
+        return;
+    } else {
+
+    }
 }
